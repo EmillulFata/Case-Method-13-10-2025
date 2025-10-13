5d82da489f6078fb9f25385adfb6f502d97467a2
FataRach <emillulfr@gmail.com>
2025-10-13 12:17:12 +0700

    Step 1

---
+++ CaseMethod/EmillulFata07.java
@@ -0,0 +1,32 @@
+package CaseMethod;
+import java.util.Scanner;
+
+public class EmillulFata07 {
+  public static void main(String[] args) {
+    double tugas, uts, uas, Nilai_Tugas, Nilai_UTS, Nilai_UAS;
+    Scanner mean = new Scanner(System.in);
+
+    System.out.println("===== INPUT DATA MAHASISWA =====");
+    System.out.println("Nama             : Emillul Fata Rachman");
+    System.out.println("NIM              : 254107060105");
+    System.out.println("Absen dan Kelas  : 07 , SIB 1F");
+
+    System.out.println("\n----- MATKUL 1 : ALGORITMA DAN PEMROGRAMAN -----");
+    System.out.print("Input Nilai Tugas : ");
+    tugas = mean.nextDouble();
+    System.out.print("Input Nilai UTS   : ");
+    uts = mean.nextDouble();
+    System.out.print("Input Nilai UAS   : ");
+    uas = mean.nextDouble();
+
+    System.out.println("\n----- MATKUL 2 : STRUKTUR DATA -----");
+    System.out.print("Input Nilai Tugas : ");
+    tugas = mean.nextDouble();
+    System.out.print("Input Nilai UTS   : ");
+    uts = mean.nextDouble();
+    System.out.print("Input Nilai UAS   : ");
+    uas = mean.nextDouble();
+
+
+  }
+}
